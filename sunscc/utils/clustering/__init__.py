1b5e957bb528db2dd7f5e3de4840135e85078154
Niels Sayez <niels.sayez@uclouvain.be>
2024-05-10 14:38:54 +0200

    Adding/Removing __init__ files to make sunscc module consistent

---
+++ sunscc/utils/clustering/__init__.py
@@ -0,0 +1,7 @@
+from .Class2Bbox import *
+from .clustering_compute_huge_dict import *
+from .clustering_compute_image_outdict import *
+from .clustering_utilities import *
+from .matching_utilities import *
+from .MeanShift import *
+from .tracking_utilities import *
---
+++ sunscc/utils/segmentation/__init__.py
@@ -0,0 +1,5 @@
+from create_detection_masks import *
+from Otsu_Grid_utilities import *
+from Otsu_local_script import *
+from Otsu_Local_utilities import *
+from parallelized_script import *
